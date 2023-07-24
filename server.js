2570ec7a059f4603651c3cf9fa96738ffe319761
wdawadwda <wdawadwdawork@gmail.com>
2023-07-24 19:29:36 +0300

    feat: only api

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override')
-const postsRouter = require('./routes/posts-routes')
-const contactsRouter = require('./routes/contacts-routes')
-const createPath = require('./helpers/createPath');
 const postApiRoutes = require('./routes/api-posts-routes');
 require('dotenv').config();
-
-const db = ''
-
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -21,35 +13,12 @@ mongoose
   .catch((error) => console.error('Ошибка подключения к базе данных:', error));
 
 const app = express();
-app.set('view engine', 'ejs')
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'style')));
-app.use(methodOverride('_method'))
+app.use(postApiRoutes);
 
 app.listen(process.env.PORT, (error) => {
   error ? console.log(error) : console.log(`Listening on port ${process.env.PORT}`);
-});
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-app.use(express.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => {
-  const title = 'Home'
-  const basePath = createPath('index');
-  res.status(200).render(basePath, { title });
-});
-
-app.use(contactsRouter);
-app.use(postsRouter);
-app.use(postApiRoutes);
-
-app.get('/about-us', (req, res) => {
-  res.redirect('/contacts');
-});
-
-app.use('*', (req, res) => {
-  const title = '404 Not Found'
-  const basePath = createPath('error');
-  res.status(404).render(basePath, { title });
 });
